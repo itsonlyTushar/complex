c617528b8a3c95079ad414bca16762e88c9f745f
itsonlyTushar <tushargsoni17@gmail.com>
2026-04-18 21:38:45 +0530

    update: updated orders page

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,5 +1,53 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Check, X } from "lucide-react";
+
 export default function OrdersPage() {
   return (
-    <div>Orders Management</div>
+    <>
+      <section className="my-4 text-3xl">
+        <h1>Orders</h1>
+        <p className="text-sm mt-2 text-left">
+          Orders for you restaurant will appear here
+        </p>
+      </section>
+
+      {/* Map the orders cards here (only on-goings)  */}
+
+      <section>
+        <Card className="max-w-xs">
+          <CardHeader>
+            <CardTitle>
+              {" "}
+              <span className="font-extrabold mr-1">#Order</span>2453
+            </CardTitle>
+            <CardDescription className="text-xs">23rd May 2026</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p>LOAD THE ORDERS DETAILS HERE</p>
+            </div>
+          </CardContent>
+          <Separator />
+          <CardFooter className="flex gap-2">
+            <Button>
+              <X />
+            </Button>
+            <Button>
+              <Check />
+            </Button>
+          </CardFooter>
+        </Card>
+      </section>
+    </>
   );
-}
+}
--- src/app/admin/payments/page.tsx
+++ src/app/admin/payments/page.tsx
@@ -1,4 +1,17 @@
 export default function AdminPaymentsPage() {
-  return <div>Payments Overview</div>;
-}
+  return (
+    <>
+      <section className="my-4 text-3xl">
+        <h1>Payments</h1>
+        <p className="text-sm mt-2 text-left">
+          Complete breakdown payments and payouts
+        </p>
+      </section>
 
+      {/* Data table seems approprtie here to show the complete payments details  */}
+      <section>
+        
+      </section>
+    </>
+  );
+}
