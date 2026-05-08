50e32fd542ca985860446592ac63b84f3a29081a
itsonlyTushar <tushargsoni17@gmail.com>
2026-05-08 23:30:00 +0530

    update (client) : court route settings pages updated

---
+++ client/src/app/court/settings/layout.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const settingsNavItems = [
+  {
+    title: "General",
+    href: "/court/settings",
+  },
+  {
+    title: "Account",
+    href: "/court/settings/accounts",
+  },
+  {
+    title: "Payments",
+    href: "/court/settings/payments",
+  },
+];
+
+export default function SettingsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0 p-6">
+      <aside className="lg:w-1/5 border px-2 py-4 rounded-xl shadow ">
+        <nav className="flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1">
+          {settingsNavItems.map((item) => (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={cn(
+                "rounded-md px-3 py-2 text-sm font-medium hover:bg-accent",
+                pathname === item.href ? "bg-accent" : "transparen",
+              )}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      </aside>
+      <div className="flex-1 lg:max-w-2xl">{children}</div>
+    </div>
+  );
+}
--- client/src/app/court/settings/page.tsx
+++ client/src/app/court/settings/page.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
 
-function page() {
-  return <div>page</div>;
+export default function AdminSettingsPage() {
+  return (
+    <section className="bg-card rounded-xl border shadow-sm p-6 h-full">
+      <h1 className="text-3xl font-bold tracking-tight mb-8">General</h1>
+
+      {/* Close store switches   */}
+      <div className="flex gap-2">
+        <div className="flex items-center gap-4 p-4 rounded-lg bg-accent/30 border border-border/50 max-w-md">
+          <Switch id="close-store" />
+          <div className="grid gap-1.5 leading-none">
+            <Label
+              htmlFor="close-store"
+              className="text-base font-semibold leading-none cursor-pointer"
+            >
+              Close Store
+            </Label>
+            <p className="text-sm text-muted-foreground">
+              Temporarily disable orders from your store.
+            </p>
+          </div>
+        </div>
+
+        {/* Notification settings  */}
+        <div className="flex items-center gap-4 p-4 rounded-lg bg-accent/30 border border-border/50 max-w-md">
+          <Switch id="close-store" />
+          <div className="grid gap-1.5 leading-none">
+            <Label
+              htmlFor="close-store"
+              className="text-base font-semibold leading-none cursor-pointer"
+            >
+              Notifications
+            </Label>
+            <p className="text-sm text-muted-foreground">
+              Change the notifications settings.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-export default page;
