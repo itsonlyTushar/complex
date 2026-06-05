f74a1a6846158fdbf489a5373053b9f52992bf23
itsonlyTushar <tushargsoni17@gmail.com>
2026-06-05 18:34:55 +0530

    fix: fixing deployment error

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 console.log("CORS middleware registered");
 app.use((req, res, next) => {
     console.log(`[CORS] ${req.method} ${req.url}`);
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL || "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
