f75401ac41caa5587e58cb8b6bd4243d524546f5
itsonlyTushar <tushargsoni17@gmail.com>
2026-06-06 13:54:30 +0530

    fix: fixing deployment issues

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log("SERVER RUNNING")
+app.listen(Number(PORT), "0.0.0.0", () => {
+    console.log(`SERVER RUNNING ON PORT: ${PORT}`);
 });
